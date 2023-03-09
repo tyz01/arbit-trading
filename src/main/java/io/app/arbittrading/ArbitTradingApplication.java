7ca6665b319b6c3e42c685257522951f533c1ee7
stanislavaleshkevich <soodostas@gmail.com>
2023-03-09 14:11:59 +0300

    init

---
+++ src/main/java/io/app/arbittrading/ArbitTradingApplication.java
@@ -0,0 +1,13 @@
+package io.app.arbittrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbitTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbitTradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/app/arbittrading/ArbitTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.app.arbittrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArbitTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
